768a3da9e9db4e9bf22b33a1b067dbe333042198
Adrian Corduneanu <kordosoft25@gmail.com>
2019-11-11 22:41:18 +0200

    Updates

--- src/modules/loan/components/dataSetup/index.jsx
+++ src/modules/loan/components/dataSetup/index.jsx
@@ -3,8 +3,51 @@ import PropTypes from 'prop-types';
 
 import './index.scss';
 
-const DataSetup = ({ text }) => {
-  return <div className="data-setup">{text}</div>;
+const DataSetup = () => {
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+
+        identityService
+            .login(login)
+            .then(() => {
+                history.push('/loanSetup');
+            })
+            .catch((error) => {
+                toast.error(error.message);
+            });
+    };
+
+    const inputOnChange = (evt) => {
+        const { name } = evt.target;
+        const { value } = evt.target;
+
+        setLogin({ ...login, ...{ [name]: value } });
+    };
+
+    return (
+        <div className="data-setup">
+            <form onSubmit={handleSubmit} className="col">
+                <Currency
+                    inputType="number"
+                    title="Loan principal"
+                    name="loanPrincipal"
+                    onChange={inputOnChange}
+                    value={dataSetup.loanPrincipal}
+                    currency={EUR}
+                />
+
+                <Percent
+                    inputType="number"
+                    title="Loan principal"
+                    name="loanPrincipal"
+                    onChange={inputOnChange}
+                    value={dataSetup.interest}
+                />
+
+                <Button type="submit" text="Next" className="btn-primary btn-block" />
+            </form>
+        </div>
+    );
 };
 
 DataSetup.propTypes = {
